5e85ce0d561b92fe88666c4c73518fdb1e739cc4
abdursoft <abdursoft@gmail.com>
2025-05-09 20:20:05 +0600

    v0.0.3

--- resources/views/pages/user/course.blade.php
+++ resources/views/pages/user/course.blade.php
@@ -46,6 +46,10 @@
             <button type="button" class="btn bg-blue-600 text-white rounded-md cursor-pointer p-2" onClick="addModule()">Add Module</button>
             <button type="submit" class="btn bg-green-600 text-white rounded-md cursor-pointer p-2" >Submit Course</button>
         </div>
+
+        <div class="flex items-center justify-center p-3 spinner hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M2,12A11.2,11.2,0,0,1,13,1.05C12.67,1,12.34,1,12,1a11,11,0,0,0,0,22c.34,0,.67,0,1-.05C6,23,2,17.74,2,12Z"><animateTransform attributeName="transform" dur="0.6s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path></svg>
+        </div>
     </form>
 
     <script>
@@ -129,7 +133,7 @@
 
         document.getElementById('courseForm').addEventListener('submit', function(e) {
             e.preventDefault();
-
+            $('.spinner').removeClass('hidden');
             const formData = new FormData(this);
 
             fetch('/user/course-action', {
@@ -138,6 +142,7 @@
                 })
                 .then(res => res.json())
                 .then(data => {
+                    $('.spinner').addClass('hidden');
                     alert('Course created successfully!');
                     window.location.reload();
                 })
